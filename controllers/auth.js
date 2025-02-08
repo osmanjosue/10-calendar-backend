7195c6dabec4f41aea4bf59c0dabb2f86db53efb
osmanjosue <osmanjosue@gmail.com>
2025-02-07 20:52:11 -0600

    Seccion 23 => Clase: Express Validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,22 @@
 const { response } = require('express')
+const { validationResult } = require('express-validator')
 
 const crearUsuario = (req, res = response) => {
 
     const { name, email, password } = req.body;
 
-    if (name.length < 5) {
+    //manejo de errores
+
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
         return res.status(400).json({
             ok: false,
-            msg: 'El nombre debe de ser de 5 letras'
-        });
-    }
+            errors: errors.mapped(),
+        })
+    };
 
-    res.json({
+    res.status(201).json({
         ok: true,
         msg: 'registro',
         name,
@@ -21,6 +26,15 @@ const crearUsuario = (req, res = response) => {
 };
 
 const loginUsuario = (req, res = response) => {
+
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped(),
+        })
+    };
 
     const { email, password } = req.body;
 
