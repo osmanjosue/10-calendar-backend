6e5de0ee19f0906a4a4cf797301739b52badf80d
osmanjosue <osmanjosue@gmail.com>
2025-03-13 20:35:34 -0600

    Seccion 24 => Clase: Validar campos necesarios

--- routes/events.js
+++ routes/events.js
@@ -4,8 +4,12 @@
  */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validarCampos } = require('../middlewares/validar-campos')
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 
@@ -17,7 +21,15 @@ router.use(validarJWT);
 router.get('/', getEventos)
 
 //Crear un nuevo evento
-router.post('/', crearEvento)
+router.post(
+    '/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalizacion es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento)
 
 //Actualizar evento
 router.put('/:id', actualizarEvento);
@@ -26,3 +38,5 @@ router.put('/:id', actualizarEvento);
 router.delete('/:id', eliminarEvento);
 
 module.exports = router;
+
+/* MINUTO 2:20 */
