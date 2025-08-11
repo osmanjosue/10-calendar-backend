0e45731dafb37a1eb172e0654cbc87c04ab1be00
osmanjosue <osmanjosue@gmail.com>
2025-08-11 17:08:44 -0600

    Seccion 24 => Clases: Validar campos necesarios y Obtener el listado de los Eventos

--- controllers/events.js
+++ controllers/events.js
@@ -1,21 +1,40 @@
 const { response } = require('express');
+const Evento = require('../models/Evento');
 
-const getEventos = (req, res = response) => {
+const getEventos = async (req, res = response) => {
+
+    const eventos = await Evento.find()
+                                .populate('user', 'name');  
+
     res.json({
         ok: true,
-        msg: 'getEventos'
+        eventos
     });
 }
 
-const crearEvento = (req, res = response) => {
+const crearEvento = async (req, res = response) => {
 
-    //Verificar que tenga el evento
-    console.log(req.body);
+    const evento = new Evento(req.body);
 
-    res.json({
-        ok: true,
-        msg: 'crearEventos'
-    });
+    try {
+        evento.user = req.uid;
+
+        const eventoGuardado = await evento.save();
+
+        res.json({
+            ok: true,
+            evento: eventoGuardado
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+
+    }
+
 }
 
 const actualizarEvento = (req, res = response) => {
