42c12541f4361127af3970bcadea88c39908b1d7
osmanjosue <osmanjosue@gmail.com>
2025-08-13 18:06:56 -0600

    Seccion 24 => Clase: Eliminar Evento

--- controllers/events.js
+++ controllers/events.js
@@ -47,7 +47,7 @@ const actualizarEvento = async (req, res = response) => {
         const evento = await Evento.findById(eventoId);
 
         if (!evento) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Evento no encontrado por id'
             });
@@ -58,14 +58,14 @@ const actualizarEvento = async (req, res = response) => {
                 ok: false,
                 msg: 'No tiene privilegios para editar este evento'
             });
-        } 
+        }
 
         const nuevoEvento = {
             ...req.body,
             user: uid
         }
 
-        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true }); 
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true });
         res.json({
             ok: true,
             evento: eventoActualizado
@@ -81,11 +81,41 @@ const actualizarEvento = async (req, res = response) => {
     }
 }
 
-const eliminarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Obtener eliminarEvento'
-    });
+const eliminarEvento = async (req, res = response) => {
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await Evento.findById(eventoId);
+
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no encontrado por id'
+            });
+        }
+
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegios para eliminar este evento'
+            });
+        }
+
+        await Evento.findByIdAndDelete(eventoId);
+        res.json({
+            ok: true
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+
+    }
 }
 
 module.exports = {
